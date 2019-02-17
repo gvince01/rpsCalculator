c6644c024660a1682f8e0c20b44f85a235607de9
gvince01 <gvince01@dcs.bbk.ac.uk>
2019-02-17 13:55:03 +0000

    Upaded the main to contain some error handling, allowed the user to try to another round of calculations

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import infixutils.InfixUtils
 
+import scala.util.control.NonFatal
+
 /**
  * @author gvince01
  *
@@ -7,21 +9,50 @@ import infixutils.InfixUtils
 
 class Main extends InfixUtils with ReversePolishLogic {
 
-  def run(): Unit = {
+
+  def welcome(): Unit = {
+    println("Welcome to the Calculator.")
     println("Please input in the following format '1 + 1', Operations currently supported are + / - *")
-    val input = scala.io.StdIn.readLine("Enter your query: ")
+    println("The use of brackets is also allowed.")
+  }
 
-    inputSanityCheck(input) match {
-      case None =>
-        println("Hmm... Something went wrong, would you like to try again?")
+  def run(): Unit = {
+    try {
+      val userInput = scala.io.StdIn.readLine("Enter your query: ")
+
+      inputSanityCheck(userInput) match {
+        case None =>
+          println("Hmm... Something went wrong, would you like to try again?")
+          run()
+
+        case Some(inputArr) =>
+          val reversePolishNotationArray = convertFromInfixToReversePolish(inputArr)
+          val expressionResult = evaluateReversePolishInput(reversePolishNotationArray)
+
+          println(s"Result = $expressionResult")
+          runAgain()
+
+          def runAgain(): Unit = {
+            val userInput = scala.io.StdIn.readLine("Would you like to use the calculator again? (Y/N) ")
+
+            userInput.toLowerCase match {
+              case "y" | "yes" =>
+                run()
+
+              case "n" | "no" =>
+                println("Goodbye!")
+
+              case _ =>
+                println("Hmm... I didn't recognise that input...")
+                runAgain()
+            }
+          }
+      }
+    } catch {
+
+      case NonFatal(e) =>
+        println(s"Something went wrong... ${e.getMessage}")
         run()
-
-      case Some(inputArr) =>
-        val reversePolishForm = convertFromInfixToReversePolish(inputArr)
-
-        val result = evaluateReversePolishInput(reversePolishForm)
-
-        println(s"Result = $result")
     }
   }
 
@@ -33,6 +64,7 @@ class Main extends InfixUtils with ReversePolishLogic {
    * @return Optionally returns an error message to display to the user
    */
   def inputSanityCheck(inputStr: String): Option[List[String]] = {
+    
     Option(inputStr.split(" ").toList)
   }
   
@@ -42,6 +74,7 @@ class Main extends InfixUtils with ReversePolishLogic {
 object Main extends App {
 
   val main = new Main
+  main.welcome()
   main.run()
 
 
